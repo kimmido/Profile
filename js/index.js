9c53d14542a1878b1cdabba8c4e84713165d122f
kimmido <icecream120102@gmail.com>
2023-01-11 00:29:43 +0900

    attitude 데이터 생성 및 전환 효과

--- js/index.js
+++ js/index.js
@@ -1,20 +1,185 @@
 window.onload = function() {
 
-    // 모바일 메뉴 열고 닫기
-    const menuBt = document.querySelector(".mobile-menu-bt > .bt-box")
+    // **** attitude섹션 내용 생성 및 변경 ****
+    const $attd = document.getElementById("attitudeWrap");
+    const $attdImg = document.getElementById("img");
+    const $attdIndex = $attd.querySelector(".index");
+    const $attdTitle = $attd.querySelector(".title > h4");
+    const $attdText = $attd.querySelector(".attitude-txt > p");
+    const $attdPrev = $attd.querySelector(".prev");
+    const $attdNext = $attd.querySelector(".next");
 
-    menuBt.addEventListener("click", menuToggle);
+    const attitude = [
+        {
+            title : "RECORD",
+            txt : "기록은 습관입니다. 스티커 메모를 활용해 빠르게 기록하고 노션을 이용해 일정을 계획하고 정리합니다. 기록은 시간을 절약하고 더 가치있게 사용할 수 있도록 해줍니다."
+        },
+        {
+            title : "CURIOSITY",
+            txt : "내용 2내 용2내  용 2내용2 내용2내 용 2내용2내용2내 용2내용2내용 2 내 용2내용2 내용2내용 2내 용2내용2 내용 2내용 2내 용2내용 2내용2내 용2 내 용2내용2내 용2 내용 2내용2내용 2내용2 내용 2내용2"
+        },
+        {
+            title : "RECORD",
+            txt : "내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3 내용3"
+        }
+    ]
+
+    // 인덱스 생성
+    for(let i = 0; i < attitude.length; i++) {
+        span = document.createElement("span");
+        span.className = "click";
+        $attdIndex.appendChild(span);
+    }
+
+    let $attdCounter = 0;
+    attdConChange();
+
+    // 내용 입력 함수
+    function attdConChange() {
+        console.log($attdCounter);
+
+        [...$attdIndex.children].forEach(idx => {
+            idx == [...$attdIndex.children][$attdCounter] ?
+            idx.classList.add("on") :
+            idx.classList.remove("on") 
+        });
+        
+        $attdTitle.innerText = `${attitude[$attdCounter].title}`
+        // $attdTitle.classList.toggle("up");
+        // setTimeout(() => {
+        //     $attdTitle.classList.toggle("up");
+        // }, 600)
+        
+        $attdText.classList.toggle("up");
+        setTimeout(() => {
+            $attdText.innerText = `${attitude[$attdCounter].txt}`
+            $attdText.classList.toggle("up");
+        }, 600)
+        
+        $attdImg.classList.toggle("coverOut");
+        setTimeout(() => {
+            $attdImg.style.backgroundImage = `url('./images/attitude(${$attdCounter}).png')`;
+            $attdImg.classList.toggle("coverOut")
+        }, 500)
+    }
+
+    // 이전, 다음, 인덱스 버튼 클릭 이벤트
+    $attdPrev.onclick = function(e) {
+        e.preventDefault();
+        $attdCounter--;
+        if($attdCounter < 0) { $attdCounter = 2; }
+        attdConChange();
+    }
+
+    $attdNext.onclick = function(e) {
+        e.preventDefault();
+        $attdCounter++;
+        if($attdCounter > 2) { $attdCounter = 0; }
+        attdConChange();
+    }
+
+    $attdIndex.onclick = function(e) {
+        if(e.target.matches('span')) {
+            $attdCounter = [...$attdIndex.children].indexOf(e.target);
+            attdConChange();
+        }
+    }
+
+
+
+    // ***** skill섹션 내용요소 생성 *****
+    const body = document.querySelector("body");
+    const skillList = document.getElementById("skillList");
+    
+    const cubeInner = ["front", "back", "top", "bottom", "left", "right"]
+    const skill = { 
+        sk : ["Java script", "SASS", "HTML", "React", "JQuery", "CSS", "Ps", "Ai"],
+        desc : [
+            "", 
+            "", 
+            "표준에 맞추어 html을 구성하고 접근성을 고려하여 작성할 수 있습니다. 시멘틱 태그를 적절히 사용하며, 태그의 용도를 알기 쉽게 클래스 이름을 부여합니다.", 
+            "", 
+            "", 
+            "", 
+            "", 
+            ""
+        ]
+    }
+
+    // skill큐브 리스트 생성
+    let cube = "";
+
+    skill.sk.forEach(sk => {
+        cube += '<div class="perspective">';
+        cube += '<div class="cube click">';
+        cubeInner.forEach(inner => {
+            inner == "front" || inner == "back" ?
+            cube += `<div class=${inner}><span>${sk}</span></div>` :
+            cube += `<div class=${inner}></div>`
+        });
+        cube += '</div>';
+        cube += '</div>';
+    });
+    skillList.innerHTML = `${cube}`;
+
+
+
+    // ********** 헤더 클릭 이벤트 ********** 
+    const gnbA = document.querySelectorAll(".gnb li a");
+    
+    let target;
+    let targetTop = [];
+    let targetBottom = [];
+ 
+    gnbA.forEach(a => {
+        target = a.getAttribute('href');
+        targetTop.push(document.querySelector(target).offsetTop);
+        targetBottom.push(document.querySelector(target).offsetTop + document.querySelector(target).offsetHeight - 1); 
+        console.log(target, document.querySelector(target).offsetTop, document.querySelector(target).offsetTop + document.querySelector(target).offsetHeight - 1);
+    });
+     
+    // gnb 클릭시 부드러운 이동
+    for(let i = 0; i < gnbA.length; i++) {
+        gnbA[i].addEventListener("click", function(e) {
+            // console.log(e);
+            console.log(this); 
+            // console.log(target);
+            e.preventDefault();
+            window.scrollTo({ left: 0, top: targetTop[i] + 1, behavior: "smooth" });
+        });
+    }
+     
+    // 로고 클릭시 맨 위로 이동
+    const logo = document.querySelector(".logo > a");
+     
+    logo.onclick = function(e) {
+        e.preventDefault();
+        window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+    };
+    // logo.addEventListener("click", (e) => {
+    //     e.preventDefault();
+    //     console.log(e.target);
+    //     console.log(e.currentTarget);
+    //     window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+    // });
+
+
+
+    // ***** 모바일 메뉴 열고 닫기 *****
+    const mMenuBt = document.querySelector(".mobile-menu-bt > .bt-box")
+    const mGnb = document.getElementById("mobileGnb");
+
+    mMenuBt.onclick = function() { menuToggle(); }
+    mGnb.onclick = function() { menuToggle(); };
 
     function menuToggle() {
-        let Mgnb = this.parentNode.nextSibling.nextSibling;
-        
-        this.classList.toggle('on');
-        Mgnb.classList.toggle('on');
+        mMenuBt.classList.toggle('on');
+        mGnb.classList.toggle('on');
     } 
 
 
 
-    // ********** 마우스 포인터 이벤트 **********
+    // ********** 마우스 포인터 효과 **********
     const home = document.getElementById("homeWrap");
     const mainCursor = document.getElementById("cursor");
     const homeCursor = document.querySelector(".mouse-pointer");
@@ -69,46 +234,7 @@ window.onload = function() {
     }
     
 
-    // ********** 헤더 클릭 이벤트 ********** 
-    const gnbA = document.querySelectorAll(".gnb li a");
     
-    let target;
-    let targetTop = [];
-    let targetBottom = [];
-
-    gnbA.forEach(a => {
-        target = a.getAttribute('href');
-        targetTop.push(document.querySelector(target).offsetTop);
-        targetBottom.push(document.querySelector(target).offsetTop + document.querySelector(target).offsetHeight); 
-    });
-    
-    // gnb 클릭시 부드러운 이동
-    for(let i = 0; i < gnbA.length; i++) {
-        gnbA[i].addEventListener("click", function(e) {
-            // console.log(e);
-            console.log(this); 
-            // console.log(target);
-            e.preventDefault();
-            window.scrollTo({ left: 0, top: targetTop[i] + 1, behavior: "smooth" });
-        });
-    }
-    
-    const logo = document.querySelector(".logo > a");
-    
-    // 로고 클릭시 맨 위로 이동
-    logo.onclick = function(e) {
-        e.preventDefault();
-        window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
-    };
-    // logo.addEventListener("click", (e) => {
-    //     e.preventDefault();
-    //     console.log(e.target);
-    //     console.log(e.currentTarget);
-    //     window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
-    // });
-
-
-
     // ********** 스크롤 이벤트 ********** 
     window.addEventListener("scroll", scrollEvent);
     
@@ -118,6 +244,7 @@ window.onload = function() {
 
     function scrollEvent() {
         pos = document.documentElement.scrollTop;
+
         backColorChange();
         currentLocation();
         itemUp();
@@ -169,13 +296,20 @@ window.onload = function() {
         }
     }
 
-    // attitude 이미지 커버 여닫기
+    // attitude섹션 이미지 커버 여닫기
     function attitudeImgCover() {
-        pos > $attd.offsetTop ? 
-        $attdImg.classList.add("coverOut") : $attdImg.classList.remove("coverOut")
+        if(pos > $attd.offsetTop) {
+            $attdTitle.classList.add("up");
+            $attdText.classList.add("up");
+            $attdImg.classList.add("coverOut");
+        } else {
+            $attdTitle.classList.remove("up");
+            $attdText.classList.remove("up");
+            $attdImg.classList.remove("coverOut");
+        }
     }
     
-    // 포트폴리오 등장 이벤트
+    // portfolio섹션 등장 이벤트
     const itemWrap = document.querySelectorAll(".portfolio-list .item-wrap");
     let item;
     let upPos;
@@ -192,106 +326,10 @@ window.onload = function() {
     
     
     
-    // **** attitude섹션 인덱스와 버튼 클릭 시 내용 변경 ****
-    const $attd = document.getElementById("attitudeWrap");
-    const $attdImg = document.getElementById("img");
-    const $attdIndex = $attd.querySelector(".index");
-    const $attdTitle = $attd.querySelectorAll(".title > li");
-    const $attdTexT = $attd.querySelectorAll(".attitude-txt > li");
-    const $attdPrev = $attd.querySelector(".prev");
-    const $attdNext = $attd.querySelector(".next");
-    let $attdCounter = 0;
     
-    // 이전, 다음, 인덱스 버튼 클릭 이벤트
-    $attdPrev.onclick = function(e) {
-        e.preventDefault();
-        $attdCounter--;
-        if($attdCounter < 0) { $attdCounter = 2; }
-        attdConChange();
-    }
-    
-    $attdNext.onclick = function(e) {
-        e.preventDefault();
-        $attdCounter++;
-        if($attdCounter > 2) { $attdCounter = 0; }
-        attdConChange();
-    }
-
-    $attdIndex.addEventListener("click", paging);
-
-    function paging(e) {
-        if(e.target.matches('span')) {
-            $attdCounter = [...$attdIndex.children].indexOf(e.target);
-            attdConChange();
-        }
-    }
-
-    // 내용 변경 함수
-    function attdConChange() {
-        console.log($attdCounter);
-
-        [...$attdIndex.children].forEach(idx => {
-            idx == [...$attdIndex.children][$attdCounter] ?
-            idx.classList.add("on") :
-            idx.classList.remove("on") 
-        });
-        
-        $attdTexT.forEach(txt => {
-            txt == $attdTexT[$attdCounter] ?
-            txt.classList.add("on") :
-            txt.classList.remove("on")
-        });
-
-        $attdTitle.forEach(title => {
-            title == $attdTitle[$attdCounter] ?
-            title.classList.add("on") :
-            title.classList.remove("on")
-        });
-
-        $attdImg.classList.toggle("coverOut");
-        setTimeout(() => {
-            $attdImg.style.backgroundImage = `url('./images/attitude(${$attdCounter}).png')`;
-            $attdImg.classList.toggle("coverOut")
-        }, 500)
-    }
-
 
     
     // ***** skill섹션 모달 기능 *****
-    const body = document.querySelector("body");
-    const skillList = document.getElementById("skillList");
-    
-    const cubeInner = ["front", "back", "top", "bottom", "left", "right"]
-    const skill = { 
-        sk : ["Java script", "SASS", "HTML", "React", "JQuery", "CSS", "Ps", "Ai"],
-        desc : [
-            "", 
-            "", 
-            "표준에 맞추어 html을 구성하고 접근성을 고려하여 작성할 수 있습니다. 시멘틱 태그를 적절히 사용하며, 태그의 용도를 알기 쉽게 클래스 이름을 부여합니다.", 
-            "", 
-            "", 
-            "", 
-            "", 
-            ""
-        ]
-    }
-
-    // skill큐브 리스트 생성
-    let cube = "";
-
-    skill.sk.forEach(sk => {
-        cube += '<div class="perspective">';
-        cube += '<div class="cube click">';
-        cubeInner.forEach(inner => {
-            inner == "front" || inner == "back" ?
-            cube += `<div class=${inner}><span>${sk}</span></div>` :
-            cube += `<div class=${inner}></div>`
-        });
-        cube += '</div>';
-        cube += '</div>';
-    });
-    skillList.innerHTML = `${cube}`;
-
     // skill설명 모달창 열기
     const skillDetail = document.getElementById("skillDetail");
     const detailClose = skillDetail.querySelector(".close");
@@ -362,6 +400,8 @@ window.onload = function() {
         this.setAttribute("src", "./images/email.svg");
     })
 
+
+    
     // 내용 변경 함수
         // [...indexArea.children].forEach(idx => {
         //     idx == [...indexArea.children][$attdCounter] ?
