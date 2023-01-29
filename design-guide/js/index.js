4790f5d55a595c3ccfddec80959bca371d026227
kimmido <icecream120102@gmail.com>
2023-01-30 00:32:10 +0900

    절물 디자인 가이드 완성

---
+++ design-guide/js/index.js
@@ -0,0 +1,15 @@
+$(function() {
+    let $target,$val,$img,$src;
+
+    $('#wireframe, #grid').on("click", function() {
+        $target = $(this).attr("id");
+        $val = $target == "wireframe"? "grid":"wireframe";
+        $img = $('.wireframe-grid-img img');
+        $src = $img.attr("src");
+
+        $img.attr("src", $src.replace($val, $target));
+        $(this)
+        .addClass("on")
+        .siblings().removeClass("on");
+    });
+});
